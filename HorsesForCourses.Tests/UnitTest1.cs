4fc3c21e7f7468861956861168d2bdde9113676f
free2c0ma <75183012+saqniok@users.noreply.github.com>
2025-07-10 16:45:09 +0200

    delete test

--- HorsesForCourses.Tests/UnitTest1.cs
+++ HorsesForCourses.Tests/UnitTest1.cs
@@ -1,12 +1,1 @@
-﻿// using HorsesForCourses.Core;
-
-// namespace HorsesForCourses.Tests;
-
-// public class UnitTest1
-// {
-//     [Fact]
-//     public void Test1()
-//     {
-//         var courseQssignedToCoach = new Course("Course One", new Period(DateTime.MinValue, DateTime.MaxValue));
-//     }
-// }
+﻿
