32e3453af7314d58b7739980ccf40e3d4c78622d
free2c0ma <75183012+saqniok@users.noreply.github.com>
2025-07-10 11:37:01 +0200

    stuck

--- HorsesForCourses.Core/Class1.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace HorsesForCourses.Core;
-
-public class Class1
-{
-
-}
---
+++ HorsesForCourses.Core/Course.cs
@@ -0,0 +1,12 @@
+﻿namespace HorsesForCourses.Core;
+
+public record Period 
+public class Course
+{
+    public string Name { get; private set; }
+    public DateTime StartTime { get; private set; }
+    public DateTime EndTime { get; private set; }
+
+    public List<Lesson> Lessons { get; private set; } = new();
+
+}
